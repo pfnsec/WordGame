2ffd1a89d41a52ec74155612dc77fe7a4b4d07e4
Peter Sherman <psherman@qti.qualcomm.com>
2020-01-21 12:35:04 +0000

    Seperate styles into a file

---
+++ src/components/Styles.js
@@ -0,0 +1,39 @@
+
+const Styles = theme => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  toolbarButtons: {
+    marginLeft: "auto",
+    marginRight: 0,
+    overflow: "hidden"
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+});
+
+export default Styles;
