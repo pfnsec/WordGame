8bd35eec2506f7aba3023d2748d3fda1223a21f3
Peter Sherman <psherman@qti.qualcomm.com>
2020-01-21 15:34:12 +0000

    Main page footer + styles

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -11,51 +11,18 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+
+import Styles from './Styles'
 import { withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
+
+import Footer from './Footer'
 
 import { withRouter } from 'react-router-dom'
 import { graphql, compose } from 'react-apollo'
 import  { gql } from 'apollo-boost'
 import { AUTH_TOKEN } from '../constant'
-
-const styles = theme => ({
-  icon: {
-    marginRight: theme.spacing(2),
-  },
-  toolbarButtons: {
-    marginLeft: "auto",
-    marginRight: 0,
-    overflow: "hidden"
-  },
-  heroContent: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6),
-  },
-  heroButtons: {
-    marginTop: theme.spacing(4),
-  },
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-  },
-  card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  cardMedia: {
-    paddingTop: '56.25%', // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
-  },
-});
 
 
 class MainPage extends Component {
@@ -70,18 +37,6 @@ class MainPage extends Component {
     }
 
 
-    Copyright() {
-      return (
-        <Typography variant="body2" color="textSecondary" align="center">
-          {'Copyright © '}
-          <Link color="inherit" href="https://material-ui.com/">
-            Word-Game
-          </Link>{' '}
-          {new Date().getFullYear()}
-          {'.'}
-        </Typography>
-      );
-    }
 
 
     cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -147,51 +102,9 @@ class MainPage extends Component {
                   </Container>
                 </div>
                 <Container className={classes.cardGrid} maxWidth="md">
-                  {/* End hero unit */}
-                  {/*
-                  <Grid container spacing={4}>
-                    {this.cards.map(card => (
-                      <Grid item key={card} xs={12} sm={6} md={4}>
-                        <Card className={classes.card}>
-                          <CardMedia
-                            className={classes.cardMedia}
-                            image="https://source.unsplash.com/random"
-                            title="Image title"
-                          />
-                          <CardContent className={classes.cardContent}>
-                            <Typography gutterBottom variant="h5" component="h2">
-                              Heading
-                            </Typography>
-                            <Typography>
-                              This is a media card. You can use this section to describe the content.
-                            </Typography>
-                          </CardContent>
-                          <CardActions>
-                            <Button size="small" color="primary">
-                              View
-                            </Button>
-                            <Button size="small" color="primary">
-                              Edit
-                            </Button>
-                          </CardActions>
-                        </Card>
-                      </Grid>
-                    ))}
-                  </Grid>
-                    */}
                 </Container>
               </main>
-              {/* Footer */}
-              <footer className={classes.footer}>
-                <Typography variant="h6" align="center" gutterBottom>
-                  Footer
-                </Typography>
-                <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-                  What are you waiting for?
-                </Typography>
-                <this.Copyright />
-              </footer>
-              {/* End footer */}
+              <Footer/>
             </React.Fragment>
           )
     }
@@ -207,8 +120,8 @@ const ME_QUERY = gql`
 `
 
 export default compose(
-    withStyles(styles),
     graphql(ME_QUERY, { name: 'meQuery' }),
+    withStyles(Styles),
   )(
   withRouter(MainPage),
 )
